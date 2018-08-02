e6a21ba1fe125679c45387694134a05492eb9553
남재우(Peter) <njw0619@sk.com>
2018-08-02 17:42:16 +0900

    initial

---
+++ src/main/java/com/fcsdm/sdmserver/SdmServerApplication.java
@@ -0,0 +1,12 @@
+package com.fcsdm.sdmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdmServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fcsdm/sdmserver/SdmServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fcsdm.sdmserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SdmServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
